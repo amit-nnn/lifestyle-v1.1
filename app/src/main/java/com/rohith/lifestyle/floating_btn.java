b3e010d0f0087c4b91af67b7f28126cee1e1baca
amit <amith.shambuni@gmail.com>
2021-04-11 13:49:14 +0530

    11th april afternoon commit with goal ui complete and remainder and task ui completed

---
+++ app/src/main/java/com/rohith/lifestyle/floating_btn.java
@@ -0,0 +1,14 @@
+package com.rohith.lifestyle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class floating_btn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floating_btn);
+    }
+}
